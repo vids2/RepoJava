a4d7aac51c5f8d42118c06a5ae4d78d7c0792211
Vidya <m2vidya.s1@gmail.com>
2020-11-08 16:12:21 +0530

    Second Commit

---
+++ PractoJavaEx/src/PractVarEx.java
@@ -0,0 +1,12 @@
+
+public class PractVarEx {
+	static {
+		System.out.println("I'm Static Block");
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!!!!");
+	}
+
+}
